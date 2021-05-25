05800b79d3da552ee3af83a51904e59304137f39
ysk8hori <ysk8.hori@gmail.com>
2021-05-25 21:53:39 +0900

    fizzbuzzにしてみた

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
 fn main() {
-    println!("Hello, world!");
-    another_function();
+    println!("{}", fizz_buzz(1));
+    println!("{}", fizz_buzz(3));
+    println!("{}", fizz_buzz(5));
+    println!("{}", fizz_buzz(15));
 }
 
-fn another_function() {
-    println!("Hello, another functions!!!!!!");
+fn fizz_buzz(number: u8) -> String {
+    match number {
+        is_fizz => "fizz".to_string();
+    }
+    if is_fizz(number) && is_buzz(number) {
+        return "FizzBuzz".to_string();
+    }
+    if is_fizz(number) {
+        return "fizz".to_string();
+    }
+    if is_buzz(number) {
+        return "buzz".to_string();
+    }
+    return number.to_string();
+}
+
+fn is_fizz(number: u8) -> bool {
+    return number % 3 == 0;
+}
+
+fn is_buzz(number: u8) -> bool {
+    return number % 5 == 0;
 }
